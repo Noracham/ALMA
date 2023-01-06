b8d34822e6c5b844e2a787e6bf40d1741fd8c6a3
Nora <nsorym04@gmail.com>
2023-01-06 20:57:22 +0900

    prism.js導入

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 $(function(){
-	// document.querySelector(".copy_button").onclick = () => navigator.clipboard.writeText("コピーさせたい文字列");
 	$(".copy_button").on("click",function(){
-		navigator.clipboard.writeText("コピー成功");
+		console.log("クリックされました");
+		$(".open h1").select();
+		document.execCommand("copy");
 	})
 })

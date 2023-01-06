63785eae93d9c73850caf0e618f5a9c2b2179ee9
Nora <nsorym04@gmail.com>
2023-01-06 22:21:54 +0900

    リストクリックでソースコードを表示

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 $(function(){
-	$(".copy_button").on("click",function(){
+	let change = false;
+	$(".open").on("click",function(){
+		const index = $("li.open").index(this);
 		console.log("クリックされました");
-		$(".open h1").select();
-		document.execCommand("copy");
+
+		if(change ==  false){
+			$(".change").eq(index).show("fast");
+			change = true;
+		}else{
+			$(".change").eq(index).hide("fast");
+			change = false;
+		}
+		// $(".open h1").select();
+		// document.execCommand("copy");
+
 	})
 })

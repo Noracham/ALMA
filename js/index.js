4a4136479045eee727446b6ab632346f570c31ae
Nora <nsorym04@gmail.com>
2023-01-07 12:40:44 +0900

    配列で真偽判断に変更

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 $(function(){
 	let change = false;
+	let cg_ary = [0,0,0,0,0,0,0,0,0,0];
+
 	$(".open").on("click",function(){
 		const index = $("li.open").index(this);
-		console.log("クリックされました");
+		console.log(index);
 
-		if(change ==  false){
+		if(cg_ary[index] ==  0){
 			$(".change").eq(index).show("fast");
-			change = true;
+			cg_ary[index] = 1;
 		}else{
 			$(".change").eq(index).hide("fast");
-			change = false;
+			cg_ary[index] = 0;
 		}
 		// $(".open h1").select();
 		// document.execCommand("copy");
